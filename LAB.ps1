be0de1e9700b9c76924e714cd9917066cdd7ca9b
Andreas Lorensen <andlo@outlook.dk>
2016-11-22 18:27:28 +0100

    .

--- LAB.ps1
+++ LAB.ps1
@@ -6,10 +6,10 @@ New-LabDefinition -Name $labName -DefaultVirtualizationEngine HyperV
 
 #Setup Network
 Add-LabVirtualNetworkDefinition -Name $labName
-Add-LabVirtualNetworkDefinition -Name Internet -HyperVProperties @{ SwitchType = 'External'; AdapterName = 'Ethernet 3' }
+#Add-LabVirtualNetworkDefinition -Name Internet -HyperVProperties @{ SwitchType = 'External'; AdapterName = 'Ethernet 3' }
 
 #Add needed ISO's
-Add-LabIsoImageDefinition -Name Exchange2016 -Path $labSources\ISOs\ExchangeServer2016-x64-cu3.iso
+#Add-LabIsoImageDefinition -Name Exchange2016 -Path $labSources\ISOs\ExchangeServer2016-x64-cu3.iso
 
 #defining default parameter values, as these ones are the same for all the machines
 $PSDefaultParameterValues = @{
@@ -29,14 +29,16 @@ $postInstallActivity = Get-LabPostInstallationActivity -ScriptFileName PrepareRo
 Add-LabMachineDefinition -Name DC1 -Roles RootDC -Processors 2 -PostInstallationActivity $postInstallActivity
 
 # DC2
-Add-LabMachineDefinition -Name DC2 -Roles DC  -OperatingSystem 'Windows Server 2016 SERVERSTANDARDCORE'
+#Add-LabMachineDefinition -Name DC2 -Roles DC 
+#-OperatingSystem 'Windows Server 2016 SERVERSTANDARDCORE'
 
 # GW
 # The GW is just a WindowsServer whith to Networkinterfaces and the Routing role
-$netAdapter = @()
-$netAdapter += New-LabNetworkAdapterDefinition -VirtualSwitch $labName
-$netAdapter += New-LabNetworkAdapterDefinition -VirtualSwitch Internet -UseDhcp
-Add-LabMachineDefinition -Name GW -Roles Routing -NetworkAdapter $netAdapter -OperatingSystem 'Windows Server 2016 SERVERSTANDARDCORE'
+#$netAdapter = @()
+#$netAdapter += New-LabNetworkAdapterDefinition -VirtualSwitch $labName
+#$netAdapter += New-LabNetworkAdapterDefinition -VirtualSwitch Internet -UseDhcp
+#Add-LabMachineDefinition -Name GW -Roles Routing -NetworkAdapter $netAdapter 
+#-OperatingSystem 'Windows Server 2016 SERVERSTANDARDCORE'
 
 # EXCH1
 # First Exchangeserver
@@ -44,16 +46,16 @@ $role = Get-LabMachineRoleDefinition -Role Exchange2016 -Properties @{
   OrganizationName       = 'LAB'
   DependencySourceFolder = "$labSources\SoftwarePackages"
 }
-Add-LabMachineDefinition -Name EXCH1 -Roles $role -MaxMemory 4GB -Processors 2
+Add-LabMachineDefinition -Name EXCH1 -Roles $role -MaxMemory 4GB -Processors 2 
 
 # NAV2016
 # A windowsserver whith NAVDEMO setup
-Add-LabMachineDefinition -Name NAV2016 -MaxMemory 2GB -Processors 1
 $postInstallActivity = Get-LabPostInstallationActivity -ScriptFileName navdemo.ps1 -DependencyFolder "$labSources\SoftwarePackages\Dynamics.90.NA.1769452.DVD" -KeepFolder
+Add-LabMachineDefinition -Name NAV2016 -MaxMemory 2GB -Processors 1 -PostInstallationActivity $postInstallActivity
 
 # Client1
 # W Windows10 based Client
-Add-LabMachineDefinition -Name Client1 -MaxMemory 2GB -OperatingSystem 'Windows 10 Enterprise'
+#Add-LabMachineDefinition -Name Client1 -MaxMemory 2GB -OperatingSystem 'Windows 10 Enterprise'
 
 Install-Lab 
 
